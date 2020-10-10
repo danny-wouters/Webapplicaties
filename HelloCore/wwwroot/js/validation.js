31179bcba7d72338cc4fecbfafa05771f093d1ca
Danny Wouters <danny_wouters9@hotmail.com>
2020-10-10 15:44:57 +0200

    Use comma as decimal operator in jquery validation

---
+++ HelloCore/wwwroot/js/validation.js
@@ -0,0 +1,4 @@
+﻿// https://stackoverflow.com/questions/48066208/mvc-jquery-validation-does-not-accept-comma-as-decimal-separator
+$.validator.methods.number = function (value, element) {
+    return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)?(?:,\d+)?$/.test(value);
+}
