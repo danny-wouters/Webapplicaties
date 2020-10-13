6d506893c58775c728e1fffc022e128e826518c2
Danny Wouters <danny_wouters9@hotmail.com>
2020-10-13 22:51:39 +0200

    Add migration for CustomUser class and properties

--- HelloCore/Migrations/20201013204638_CustomUserData.Designer.cs
+++ HelloCore/Migrations/20201013204638_CustomUserData.Designer.cs
@@ -4,14 +4,16 @@ using HelloCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HelloCore.Migrations
 {
     [DbContext(typeof(HelloCoreContext))]
-    partial class HelloCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20201013204638_CustomUserData")]
+    partial class CustomUserData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
